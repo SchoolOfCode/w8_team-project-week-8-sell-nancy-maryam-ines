07dcbbfc692f1ce61f41682f25dad7b6414fb9c8
sellanni <sellanni@hotmail.co.uk>
2021-05-20 14:59:57 +0100

    simple-plan

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-//pseudo code here
+// create template in HTML & CSS
+// follow wireframe design
+// ~ 3-4 pages, homepage, about us, contact us, potential separate translation page
+// buttons & input build first for testing
+// Testing and choosing APIs
+// Api-1 = translation
+// Api-2 = text-to-speech
+// Api-3 = ??
 //
