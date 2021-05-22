c57ec1276a26be51eb64a706335628f8be67c984
Maryam Dar <maryamdar@hotmail.com>
2021-05-22 16:17:48 +0100

    updated name and added contact button

--- main.js
+++
@@ -1,9 +0,0 @@
-// create template in HTML & CSS
-// follow wireframe design
-// ~ 3-4 pages, homepage, about us, contact us, potential separate translation page
-// buttons & input build first for testing
-// Testing and choosing APIs
-// Api-1 = translation
-// Api-2 = text-to-speech
-// Api-3 = ??
-//
