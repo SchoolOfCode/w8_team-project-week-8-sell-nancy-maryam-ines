58a1143dd1bdb9d594dc8859fa7381f23568c124
inesperez <71775778+inesperez@users.noreply.github.com>
2021-05-22 14:34:47 +0100

    Shakespeare API

--- main.js
+++ main.js
@@ -7,3 +7,30 @@
 // Api-2 = text-to-speech
 // Api-3 = ??
 //
+
+//Skakespeare API
+
+let inputElement = document.querySelector("#inputTxt");
+let translateBtnElement = document.querySelector("#translateButton"); 
+let outputElement = document.querySelector("#outputTxt"); 
+let url="https://api.funtranslations.com/translate/shakespeare.json";
+
+translateBtnElement.addEventListener("click", translateFunction);
+function translatedURL(inputValue){
+    return `${url} ?text= ${inputValue}`;   
+}
+
+function translatedURL(inputValue){
+    return `${url}?text=${inputValue}`;  
+}
+
+function translateFunction(event){
+    let inputValue = inputElement.value;    
+    let finalURL = translatedURL(inputValue);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            outputElement.innerText = json.contents.translated;
+        })
+        .catch(() => alert("Shakespeare(Server) is busy! Try after sometime"))
+}
